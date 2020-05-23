be03e332cba404832604c661ac32d8573418c335
jithumajinu <jithujithupm@gmail.com>
2020-05-23 23:35:27 +0900

    Update shellscript.sh

--- src/shellscript.sh
+++ src/shellscript.sh
@@ -1,1 +1,9 @@
 
+To list 
+=> echo " PORT : 8888 | process : $(lsof -i:8888 -t)"
+
+=> lsof -nP -iTCP -sTCP:LISTEN | grep 8888 
+
+To kill a process running on a port.
+Ex running on port 8888.
+=> kill -9 $(lsof -i:8888 -t) 2> /dev/null
